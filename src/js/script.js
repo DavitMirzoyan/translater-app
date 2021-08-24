09022868595123ab687f86ce4d51747da01be4ea
Dsvit Mirzoyan <davit.mirzoyan.19@ucl.ac.uk>
2021-08-24 12:22:10 +0400

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,65 +1,100 @@
+//const { systemPreferences } = require("electron");
+
+
 document.addEventListener('DOMContentLoaded', function () {
+
+    
+    import { systemPreferences } from "electron/main";
 
     let startButton = document.getElementById('startButton');
     let timer = document.createElement("div");
-    let element = document.getElementById('timerContainer')
+    let element = document.getElementById('timerContainer');
 
-    timer.setAttribute('id', 'timer')
-    let timerStarted = false
+    timer.setAttribute('id', 'timer');
+    let timerStarted = false;
 
-    var x;
+    let x;
 
     startButton.addEventListener('click', () => {
 
-        if (!timerStarted) {
+        let access = systemPreferences.getMediaAccessStatus("microphone");
+        let dest = document.getElementById('access');
+        dest.innerHTML = "access";
 
+     /*   window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 
-            let secondsCount = 0;
-            element.appendChild(timer).innerHTML = "00:00";
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        recognition.continuous = true;
 
-            timerStarted = true;
+        let p = document.createElement("p");
+        p.classList.add("para");
+        let words = document.querySelector(".words");
+        words.appendChild(p);
 
-            x = setInterval(() => {
-                secondsCount++;
-                timer.innerHTML = formatNumToTime(secondsCount);
-            }, 1000);
+        recognition.start();
 
-            startButton.innerHTML = 'Stop timer';
+        recognition.addEventListener("result", e => {
+            let speechToText = "";
+            let interimTranscript = '';
+            for (let i = e.resultIndex, len = e.results.length; i < len; i++) {
+                let transcript = e.results[i][0].transcript;
 
+                if (e.results[i].isFinal) {
+                    speechToText += transcript;
+                } else {
+                    interimTranscript += transcript;
+                }
 
-        } else {
-            clearInterval(x);
-            element.removeChild(timer);
-            startButton.innerHTML = 'Start timer';
-            timerStarted = false;
-        }
-
+                console.log(speechToText);
+            }
+            document.querySelector(".para").innerHTML = speechToText + interimTranscript
+        });*/
     })
+    /*
+    if (!timerStarted) {
 
 
-    const formatNumToTime = (num) => {
-        let mins = 0;
-        let seconds = num;
-        if (num > 59) {
-            seconds = num % 60;
-            mins = parseInt(num / 60);
+        let secondsCount = 0;
+        element.appendChild(timer).innerHTML = "00:00";
 
-            console.log(mins);
-        }
+        timerStarted = true;
+
+        x = setInterval(() => {
+            secondsCount++;
+            timer.innerHTML = formatNumToTime(secondsCount);
+        }, 1000);
+
+        startButton.innerHTML = 'Stop timer';
 
 
-        return `${mins < 10 ? '0' : ''}${mins}:${seconds < 10 ? '0' : ''}${seconds}`
+    } else {
+        clearInterval(x);
+        element.removeChild(timer);
+        startButton.innerHTML = 'Start timer';
+        timerStarted = false;
     }
-});
+*/
+})
 
 
-function GetSelectedText(value) {
-    console.log(value)/*
-    var e = document.getElementById("langauges");
-    var result = e.options[e.selectedIndex].text;
-    document.getElementById("result").innerHTML = result;*/
-}
+const formatNumToTime = (num) => {
+    let mins = 0;
+    let seconds = num;
+    if (num > 59) {
+        seconds = num % 60;
+        mins = parseInt(num / 60);
+
+        console.log(mins);
+    }
+
+
+    return `${mins < 10 ? '0' : ''}${mins}:${seconds < 10 ? '0' : ''}${seconds}`
+};
+
+
 window.addEventListener("load", function () {
+
     let text = document.createElement("div");
     text.setAttribute('id', 'text');
 
@@ -68,12 +103,14 @@ window.addEventListener("load", function () {
     element.appendChild(text).innerHTML = "";
 
     document.getElementById("languages").addEventListener("change", function () {
-        if (this.value == "null") {
-            return;
-        }
 
         let fullSentence = `The speaker's language is ${this.value}`;
 
         element.appendChild(text).innerHTML = fullSentence;
+
     });
 });
+
+
+
+
